426c1724b712493784732859e2871d75f078ed75
koby5i <bobkoby5i@xdeq.com>
2022-04-19 19:28:02 +0200

    POST user valiation with express-validator

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const {check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User')
 
 // @route GET api/users/hello
 router.get("/hello", async (req, res) => {
@@ -16,13 +19,22 @@ router.get("/hello", async (req, res) => {
 // @route    POST api/users
 // @desc     Register a user
 // @access   Public
-router.post("/", async (req, res) => {
-    console.log("POST /api/users in users.js")
-    console.log(req.body)
-    const p_email = req.body.email
-    const p_pass  = req.body.password    
-    console.log(`email ${p_email} password ${p_pass}`)
-    res.send("User registered");
-});
+router.post("/", [
+        check('name', 'Please enter name.').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password must be  6 or more characters').isLength({min:6}),
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        console.log("POST /api/users in users.js")
+        console.log(req.body)
+        const {email, password} = req.body
+        console.log(`email ${email} password ${password}`)
+        res.status(201).send("User registered sucessfuly. ");
+    }
+);
 
 module.exports = router;
